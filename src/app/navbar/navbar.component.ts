234c1c45ecf8afbc37bb51a5f51716953ca86b85
Sam H <sam12307@live.com>
2021-05-05 12:52:43 -0500

    Added Some Components and Modules

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  //Component to have the different buttons for the routing module 
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
