3638c5d3612117a5f521a0e112977c76261320b7
Nathan De La Garza <nathanhale.delagarza@ndsu.edu>
2021-05-07 23:01:07 -0500

    Fix app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,11 +15,8 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SearchComponent } from './search/search.component';
 import { MyCalendarsComponent } from './my-calendars/my-calendars.component';
-<<<<<<< HEAD
 import { NotesComponent } from './notes/notes.component';
-=======
 import { NewCalendarComponent } from './new-calendar/new-calendar.component';
->>>>>>> 6710b10959e5edf00be978edc597d2037b7ff06f
 
 @NgModule({
   declarations: [
@@ -31,12 +28,8 @@ import { NewCalendarComponent } from './new-calendar/new-calendar.component';
     NavbarComponent,
     SearchComponent,
     MyCalendarsComponent,
-<<<<<<< HEAD
     NotesComponent,
-=======
     NewCalendarComponent,
->>>>>>> 6710b10959e5edf00be978edc597d2037b7ff06f
-
   ],
   imports: [
     BrowserModule,
@@ -45,18 +38,18 @@ import { NewCalendarComponent } from './new-calendar/new-calendar.component';
     HttpClientModule,
 
     RouterModule.forRoot([
-      { path : 'calendar', component: CalendarComponent },
+      { path: 'calendar', component: CalendarComponent },
       { path: 'login', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
-      { path: '', redirectTo: '/register', pathMatch: 'full'},
-      { path: 'home', component: HomeComponent}
+      { path: '', redirectTo: '/register', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
     ]),
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
-    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true},
-    fakeBackendProvider
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider,
   ],
-  bootstrap: [AppComponent,]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
