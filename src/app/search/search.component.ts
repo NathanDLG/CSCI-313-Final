deead1c22b9a5b9a26e4de4bde1542e3054965fe
Nathan De La Garza <nathanhale.delagarza@ndsu.edu>
2021-05-10 22:18:39 -0500

    search by user ID implemented

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -3,76 +3,53 @@ import { UserTemplate } from '../UserTemplate';
 import { UserService } from '../_services/user.service';
 import { Observable, Subject } from 'rxjs';
 import { distinctUntilChanged } from 'rxjs/operators';
-import {CalendarsService } from '../calendars.service';
+import { CalendarsService } from '../calendars.service';
 import { CalendarTemplate } from '../CalendarTemplate';
-
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
 export class SearchComponent implements OnInit {
-
-  constructor(private service: UserService, private serviceCal : CalendarsService) { }
+  constructor(
+    private service: UserService,
+    private serviceCal: CalendarsService
+  ) {}
 
   ngOnInit(): void {
     this.userArray = this.service.getUsers();
     this.calendarArraylocal = this.serviceCal.getCalendars();
-
   }
 
-  userArray : UserTemplate[];
-  calendarArraylocal : CalendarTemplate[]; 
+  userArray: UserTemplate[];
+  calendarArraylocal: CalendarTemplate[];
+  public matchFound: boolean = false;
 
-i : number ; 
-temp : string;
-filteredArray : UserTemplate[];
-searchnum : number;
+  i: number;
+  temp: string;
+  filteredArray: UserTemplate[];
+  user$: Observable<UserTemplate[]>;
 
-user$: Observable<UserTemplate[]>;
+  coolArray: number[] = [1]; //Initialize initially to allow elements to be pushed/array reset
 
-coolArray : number[];
-
-
-  onSubmit(value : string){
-   this.searchnum = parseInt(value);
-
-
-    for(let i = 0; i < this.userArray.length; i++){
-        if(this.calendarArraylocal[i].userID === this.searchnum)
-          {
-            this.coolArray.push(this.searchnum);
-          }
-          this.coolArray.push(this.searchnum);
+  onSubmit(value: string) {
+    let searchnum: number = parseInt(value);
+    this.coolArray = [];
+    this.matchFound = false;
+    for (let i = 0; i < this.calendarArraylocal.length; i++) {
+      if (this.calendarArraylocal[i].userID == searchnum) {
+        this.coolArray.push(this.calendarArraylocal[i].calendarID);
+        this.matchFound = true;
+      }
     }
-      
-      
-
-
-
-    
-
-
   }
 
+  filter(value: string) {
+    // this.temp = this.userArray(UserTemplate.fname);
 
-  filter(value : string){
-
-  // this.temp = this.userArray(UserTemplate.fname);
-
-    
-
-    for(this.i = 0; this.i < this.userArray.length - 1 ; this.i ++){
-      
-      this.userArray.find(el => el.fname == this.temp);
-
-
+    for (this.i = 0; this.i < this.userArray.length - 1; this.i++) {
+      this.userArray.find((el) => el.fname == this.temp);
     }
-
-
-
-    
   }
-
 }
